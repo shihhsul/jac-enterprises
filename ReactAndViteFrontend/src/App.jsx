151ac4b242ccb4a5a3c13ecb2eb8b8243d6f03b5
unknown <marsandnoa@gmail.com>
2023-08-29 15:18:11 -0400

    commenting out api request:wq

--- ReactAndViteFrontend/src/App.jsx
+++ ReactAndViteFrontend/src/App.jsx
@@ -20,9 +20,6 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <div>
-      <ApiRequest />
-      </div>
     </>
   )
 }
