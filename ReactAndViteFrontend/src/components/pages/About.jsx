15a97832e49758b4aa21754f3f130f38258d7ff4
bcmgbokwere0 <bcmgbokwere@gmail.com>
2023-08-29 21:04:15 -0500

    Working on data for bios

--- ReactAndViteFrontend/src/components/pages/About.jsx
+++ ReactAndViteFrontend/src/components/pages/About.jsx
@@ -1,6 +1,25 @@
+import AboutList from "../Lists/AboutList";
+
 const About = () => {
-    return <h1>About</h1>
-}
+  const DUMMY_DATA = [
+    {
+      id: "m1",
+      name: "Brian Mgbokwere",
+      image: "ReactAndViteFrontend/src/components/photos/Brian.jpg",
+      biography:
+        "I am a graduate from Northern Illinois University, where I earned my Bachelor of Science in Health Sciences. My studies focused on environmental health, physical and mental health, and how to best deliver care to under-served communities. Despite my background, I have always had an interest in software development.",
+    },
 
+  ];
 
-export default About
+  return (
+    <section>
+      <h1>About</h1>
+      <ul>
+        <AboutList bios={DUMMY_DATA} />
+      </ul>
+    </section>
+  );
+};
+
+export default About;
