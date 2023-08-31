76c57de5021f21b4f8bc812ab6b03ad74931a5e5
shihhsul <wrtyujmoo@gmail.com>
2023-08-31 19:55:38 -0400

    Add css for main page and write some code in main.jsx

--- ReactAndViteFrontend/src/components/pages/Main.jsx
+++ ReactAndViteFrontend/src/components/pages/Main.jsx
@@ -1,10 +1,61 @@
 // import { title } from 'process'
-import  style  from './Main.css'
+import  style  from './Main.module.css'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const Main = () => { 
-    return <h1>JAC Enterprises</h1>
+const Main = () => {
+  const [nasaData, setNasaData] = useState([]);
+  const [boredData, setBoredData] = useState([]);
 
-}
+  useEffect(() => {
+    axios.get(`/api/nasa/new`)
+      .then(response => {
+        setNasaData(response.data);
+      })
+      .catch(error => {
+        console.error("Error fetching nasa data:", error);
+      });
+      
+    axios.get(`/api/bored/new`)
+      .then(response => {
+        setBoredData(response.data);
+      })
+      .catch(error => {
+        console.error("Error fetching bored data:", error);
+      });
 
+  }, []);
 
-export default Main
+  return (
+    <div className={style.container}>
+
+    <div className={style.card}>
+        <div className={style.imgbox}>
+            <img src="https://1000logos.net/wp-content/uploads/2017/03/NASA-Logo.jpg"></img>
+        </div>
+      <div className={style.textbox}>
+        <h3>Nasa</h3>
+        {nasaData ? (
+          <p>{nasaData.nasaArticles}</p>
+        ) : (
+          <p>Loading Nasa data...</p>
+        )}
+        </div>
+      </div>
+      <div className={style.card}>
+        <div className={style.imgbox}>
+            <img src="https://img.freepik.com/premium-vector/light-bulb-with-rays-shine-cartoon-style-flat-style-hand-drawn-style-doodle-style-symbol-creativity-innovation-inspiration-invention-idea-vector-illustration_175838-3305.jpg"></img>
+        </div>
+      <div className={style.textbox}>
+        <h3>Bored</h3>
+        {boredData ? (
+          <p>{boredData.boredArticles}</p>
+        ) : (
+          <p>Loading bored data...</p>
+        )}
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Main;
